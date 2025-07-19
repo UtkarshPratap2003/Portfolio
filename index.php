f9e04530128d521689fa80aa40301eaedb71a71c
Utkarsh Pratap <utkarshpratap03@gmail>
2025-07-20 00:17:40 +0530

    Contact form

--- index.php
+++ index.php
@@ -435,7 +435,7 @@
     <div class="col-md-12">
       <div class="form-group">
         <label for="subject">Subject</label>
-        <input type="text" id="subject" name="subject" class="form-control" placeholder="Your Subject" required>
+        <input type="text" id="subject" name="sub" class="form-control" placeholder="Your Subject" required>
         <label class="for-icon"><i class="far fa-user"></i></label>
       </div>
     </div>
@@ -443,7 +443,7 @@
     <div class="col-md-12">
       <div class="form-group">
         <label for="message">Your Message</label>
-        <textarea name="message" id="message" class="form-control" rows="4" placeholder="Write Your message" required></textarea>
+        <textarea name="message" id="msg" class="form-control" rows="4" placeholder="Write Your message" required></textarea>
       </div>
     </div>
 
@@ -501,3 +501,57 @@
 
 
 </html>
+<?php
+
+    //Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+ if(isset($_POST['send']))
+ {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $sub = $_POST['sub'];
+    $msg = $_POST['msg'];
+
+
+
+//Load Composer's autoloader (created by composer, not included with PHPMailer)
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'utkarshpratap03@gmail.com';            //SMTP username
+    $mail->Password   = 'upqq hdnm ahsa dkuu';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('utkarshpratap03@gmail.com', 'contact Form');
+    $mail->addAddress('harshikarora22@gmail.com', 'Our Website');           //Add a recipient
+  
+
+ 
+    //Content
+    $mail->isHTML(true);                                       //Set email format to HTML
+    $mail->Subject = 'Contact Form';
+    $mail->Body    = "Sender Name - $name <br> Sender Email - $email <br> Subject - $sub <br> message - $msg";
+
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+}
+?>
